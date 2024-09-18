b65c46d03f4fb0f28d0ee88aa42ebd13fa62a4f4
Hamjoshua <biryukov-10@list.ru>
2024-09-18 21:44:03 +0700

    Лабы

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -26,6 +31,49 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        val linearLayout = LinearLayout(this)
+        linearLayout.orientation = LinearLayout.VERTICAL
+        linearLayout.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+
+        val edit: EditText = EditText(this)
+        edit.setText("Бирюков Марк ПИ-231")
+
+        val img = ImageView(this)
+        img.setImageResource(R.mipmap.ic_launcher_round)
+
+        val textView1 = TextView(this)
+        textView1.text = "Бирюков Марк ПИ-231"
+        textView1.textSize = 18f
+
+        val btn = Button(this).apply {
+            text = "ОК"
+        }
+        btn.setOnClickListener {
+            textView1.text = edit.text
+        }
+
+        val textView2 = TextView(this)
+        textView2.text = "Если б мишки были пчелами,\n" +
+                "То они бы нипочём\n" +
+                "Никогда и не подумали\n" +
+                "Так высоко строить дом!\n"
+        textView2.textSize = 18f
+
+        val textView3 = TextView(this)
+        textView3.text = getString(R.string.bears)
+        textView3.textSize = 18f
+
+        linearLayout.addView(edit)
+        linearLayout.addView(img)
+        linearLayout.addView(btn)
+        linearLayout.addView(textView1)
+        linearLayout.addView(textView2)
+        linearLayout.addView(textView3)
+
+        setContentView(linearLayout)
     }
 }
 
